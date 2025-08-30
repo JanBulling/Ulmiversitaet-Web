427d2b4fa66b1d91663576ff0ef35eeafe2aece0
Jan Bulling <jan.greenengage@gmail.com>
2025-08-30 18:03:08 +0200

    feat: use drawer for search on mobile instead of popover

--- src/components/öpnv/stop-select-btn.tsx
+++ src/components/öpnv/stop-select-btn.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import * as React from "react";
-
-import { Popover, PopoverContent, PopoverTrigger } from "@/ui/popover";
-import { Button } from "@/ui/button";
+import { CheckIcon, ChevronsUpDownIcon } from "lucide-react";
 
 import allStops from "@/content/public-transport/allStops.json";
-import { CheckIcon, ChevronsUpDownIcon } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/ui/popover";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/ui/drawer";
+import { Button } from "@/ui/button";
 import {
   Command,
   CommandEmpty,
@@ -16,6 +16,7 @@ import {
 } from "@/ui/command";
 import { cn } from "@/lib/utils";
 import { defaultStop } from "@/content/public-transport/config";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 interface StopSelectorButtonProps {
   stopId?: number;
@@ -27,12 +28,38 @@ export default function StopSelectorButton({
   onStopIdChange,
 }: StopSelectorButtonProps) {
   const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
   const currentStop = allStops.find((stop) => stop.id === stopId);
 
+  if (isDesktop) {
+    return (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[300px] justify-between"
+          >
+            {currentStop ? currentStop.name : "Haltestelle auswählen..."}
+            <ChevronsUpDownIcon className="ml-2" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[300px] p-0">
+          <StopList
+            setOpen={setOpen}
+            onStopIdChange={onStopIdChange}
+            stopId={stopId}
+          />
+        </PopoverContent>
+      </Popover>
+    );
+  }
+
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button
           variant="outline"
           role="combobox"
@@ -42,36 +69,56 @@ export default function StopSelectorButton({
           {currentStop ? currentStop.name : "Haltestelle auswählen..."}
           <ChevronsUpDownIcon className="ml-2" />
         </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[300px] p-0">
-        <Command>
-          <CommandInput placeholder="Haltestelle suchen..." />
-          <CommandList>
-            <CommandEmpty>Haltestelle nicht gefunden.</CommandEmpty>
-            {allStops.map((stop) => (
-              <CommandItem
-                key={stop.id}
-                value={stop.name}
-                onSelect={(newValue) => {
-                  const selectedStop = allStops.find(
-                    (stop) => stop.name === newValue,
-                  );
-                  onStopIdChange?.(selectedStop?.id ?? defaultStop.stopId);
-                  setOpen(false);
-                }}
-              >
-                <CheckIcon
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    stopId === stop.id ? "opacity-100" : "opacity-0",
-                  )}
-                />
-                {stop.name}
-              </CommandItem>
-            ))}
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mt-4 border-t">
+          <StopList
+            setOpen={setOpen}
+            onStopIdChange={onStopIdChange}
+            stopId={stopId}
+          />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+}
+
+function StopList({
+  setOpen,
+  stopId,
+  onStopIdChange,
+}: {
+  setOpen: (open: boolean) => void;
+  stopId?: number;
+  onStopIdChange?: (stopId: number) => void;
+}) {
+  return (
+    <Command>
+      <CommandInput placeholder="Haltestelle suchen..." />
+      <CommandList>
+        <CommandEmpty>Haltestelle nicht gefunden.</CommandEmpty>
+        {allStops.map((stop) => (
+          <CommandItem
+            key={stop.id}
+            value={stop.name}
+            onSelect={(newValue) => {
+              const selectedStop = allStops.find(
+                (stop) => stop.name === newValue,
+              );
+              onStopIdChange?.(selectedStop?.id ?? defaultStop.stopId);
+              setOpen(false);
+            }}
+          >
+            <CheckIcon
+              className={cn(
+                "mr-2 h-4 w-4",
+                stopId === stop.id ? "opacity-100" : "opacity-0",
+              )}
+            />
+            {stop.name}
+          </CommandItem>
+        ))}
+      </CommandList>
+    </Command>
   );
 }
