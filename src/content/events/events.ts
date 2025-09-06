d0d166b3514a7a8dece44781970a70b1a98566f1
Jan Bulling <jan.greenengage@gmail.com>
2025-09-06 17:52:32 +0200

    test: testing events on vercel

--- src/content/events/events.ts
+++ src/content/events/events.ts
@@ -10,17 +10,21 @@ export type CalendarEvent = {
 
 export function getEvents(): CalendarEvent[] {
   const folder = path.join(process.cwd(), "src", "content", "events");
-  const files = fs.readdirSync(folder).filter((file) => {
-    return path.extname(file) === ".json";
-  });
+  try {
+    const files = fs.readdirSync(folder).filter((file) => {
+      return path.extname(file) === ".json";
+    });
 
-  const events = files.map((file) => {
-    const filePath = path.join(folder, file);
-    const rawContent = fs.readFileSync(filePath, "utf-8");
-    const data = JSON.parse(rawContent) as CalendarEvent[];
+    const events = files.map((file) => {
+      const filePath = path.join(folder, file);
+      const rawContent = fs.readFileSync(filePath, "utf-8");
+      const data = JSON.parse(rawContent) as CalendarEvent[];
 
-    return data;
-  });
-
-  return events.flat();
+      return data;
+    });
+    return events.flat();
+  } catch (err) {
+    console.error("[GET-EVENTS]", err);
+    return [];
+  }
 }
