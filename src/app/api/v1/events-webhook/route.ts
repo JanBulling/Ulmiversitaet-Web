155cd09c1da0accfbe8a3b4c88c4966050db5e4f
Jan Bulling <jan.greenengage@gmail.com>
2025-09-15 13:32:44 +0200

    fix: update POST webhook to handle text data and log headers

--- src/app/api/v1/events-webhook/route.ts
+++ src/app/api/v1/events-webhook/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
+  const data = await req.text();
+  const headers = req.headers;
+  console.info("[WEBHOOK - POST]", "headers", headers);
   console.info("[WEBHOOK - POST]", data);
   return new Response("success", { status: 200 });
 }
